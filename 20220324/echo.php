fb80b68272c987f1b5ab81fc1b322efc86e6374c
Vane222 <101894312+Vane222@users.noreply.github.com>
2022-03-24 17:30:26 -0500

    Add files via upload

---
+++ 20220324/echo.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <h1>Hola soy un titulo</h1>
+        <?php
+        //INICIA CODIGO PHP
+        //ESTO ES UN COMENTARIO
+        /*Todo lo que este aqui dentro
+        se considera un comentario*/
+        // CUANDO ECHO DEVUELVE UN VALOR
+        echo "Hola soy un parrafo";
+        echo "<br>";  //salto de linea
+        echo 3+3;     /* realiza operaciones en php que no se puede en html.*/
+        //TERMINA PHP
+        ?>
+    </body>
+</html>
